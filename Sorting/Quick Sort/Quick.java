9c6eea0a5004f622654bc9499a3326168e056c4d
Stephen Paul <stephenpaul4040@gmail.com>
2025-09-21 13:58:13 +0530

    Added Quick Sort

---
+++ Sorting/Quick Sort/Quick.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Quick {
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); 
+
+        for (int test = 0; test < t; test++) {
+            String[] input = sc.nextLine().split(" ");
+            int[] arr = new int[input.length];
+            for (int i = 0; i < input.length; i++) {
+                arr[i] = Integer.parseInt(input[i]);
+            }
+
+            quickSort(arr, 0, arr.length - 1);
+
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
